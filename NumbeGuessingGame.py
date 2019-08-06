2d1fbf5c1fd85f7c00619b79777f9561b93a235c
Stan Smoczyk <stansmoczyk@gmail.com>
2019-08-06 11:02:21 -0400

    Number guessing game I made to play around with learning.

---
+++ NumbeGuessingGame.py
@@ -0,0 +1,22 @@
+#This is a number guessing game.
+import random
+secretNumber = random.randint(1, 40)
+print("Hello. What is your name?")
+name = input()
+print ('Well ' + name + ', I am thinking of a number between 1 and 40')
+
+#ask for player to make 6 guess's
+for guessesTaken in range(1, 9):
+    print('Take a guess.')
+    guess = int(input())
+    if guess < secretNumber:
+        print('Your guess is too low.')
+    elif guess > secretNumber:
+        print ('Your guess is too high.')
+    else:
+        break #This condition is the correct guess!
+
+if guess == secretNumber:
+    print('Good job, ' + name + '! You guessed my number in ' + str(guessesTaken) + ' guesses!')
+else:
+    print('Nope. The number I was thinking of was ' + str(secretNumber))
