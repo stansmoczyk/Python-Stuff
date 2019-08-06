12ae46acb2582f59bfd68e11cfc4daf4470ffebe
Stan Smoczyk <stansmoczyk@gmail.com>
2019-08-06 11:18:13 -0400

    FizzBuzz in Python

---
+++ FizzBuzzPython.py
@@ -0,0 +1,12 @@
+#fizz Buzz Python
+
+for num in range (1, 100):
+    if num % 15 == 0:
+        print ("FizzBuzz")
+    elif num % 3 == 0:
+        print ("Fizz")
+    elif num % 5 == 0:
+        print ("Buzz")
+    else:
+        print (num)
+        
